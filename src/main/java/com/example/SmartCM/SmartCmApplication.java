75d4c3f15fdd2b582bc17ec1b1a7c5d631bb304c
Parth <parthwork987@gmail.com>
2026-02-10 17:10:18 +0530

    .

--- src/main/java/com/example/SmartCM/SmartCmApplication.java
+++ src/main/java/com/example/SmartCM/SmartCmApplication.java
@@ -2,7 +2,6 @@ package com.example.SmartCM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
